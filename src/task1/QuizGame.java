633eea3e8212f23ad253e1eadde919df433ac4b7
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-16 00:46:07 +0530

    List of Array created for quiz questions

--- src/task1/QuizGame.java
+++ src/task1/QuizGame.java
@@ -6,5 +6,14 @@ public class QuizGame {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        //An Array of quiz questions
+        System.out.println("Read and Answer the following Quiz Questions Correctly");
+        String[] questions = {
+                "1.) What is the smallest prime number?",
+                "2.) How many time zones are there in Russia?",
+                "3.) What is the capital of France?",
+                "4.) How many days does it take for the Earth to orbit the Sun?",
+                "5.) How many stripes are there on the US flag?",
+        };
     }
 }
