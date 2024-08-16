7a65abcb9ba30c1239270904d5cab7a5563df1ed
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-08-16 11:59:46 +0530

    update

--- src/task2/PyramidPattern.java
+++ src/task2/PyramidPattern.java
@@ -3,5 +3,20 @@ package task2;
 public class PyramidPattern {
     public static void main(String[] args) {
         int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            // Printing spaces
+            for (int j = rows - i; j > 0; j--) {
+                System.out.print(" ");
+            }
+
+            // Printing numbers
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+
+            // Move to the next line
+            System.out.println();
+
+        }
     }
 }
