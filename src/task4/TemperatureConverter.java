922c125b0b0510570f828e68ce994eb44f626db4
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-01 19:21:10 +0530

    Class created for temperature conversion

---
+++ src/task4/TemperatureConverter.java
@@ -0,0 +1,13 @@
+package task4;
+
+import java.util.Scanner;
+
+public class TemperatureConverter {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Please kindly read through and follow the instructions.");
+        System.out.println("-------------------------------------------------------");
+    }
+}
