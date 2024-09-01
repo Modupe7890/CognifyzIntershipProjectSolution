811d0887bae37e6d286c1be09714e9a703ac43e8
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-01 19:33:31 +0530

    prompt to select preferred option to convert temperature created

--- src/task4/TemperatureConverter.java
+++ src/task4/TemperatureConverter.java
@@ -9,5 +9,13 @@ public class TemperatureConverter {
 
         System.out.println("Please kindly read through and follow the instructions.");
         System.out.println("-------------------------------------------------------");
+
+        System.out.println("Please enter your Temperature: ");
+        double temp = input.nextDouble();
+
+        System.out.println("Select your preferred conversion option");
+        System.out.println("1. Fahrenheit to Celsius: ");
+        System.out.println("2. Celsius to Fahrenheit: ");
+
     }
 }
