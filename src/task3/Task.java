666a5d31389a5fcc86ad7ff94eac047cb6f73a1a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-01 16:30:38 +0530

    constructor for task created

--- src/task3/Task.java
+++ src/task3/Task.java
@@ -4,4 +4,10 @@ public class Task {
     private int id;
     private String title;
     private String description;
+
+    public Task(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
 }
