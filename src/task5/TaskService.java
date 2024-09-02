a910af7941d4fef90464873fbefa95a5d9686b8a
Modupe Oyedeji <oyedejimodupegrace@gmail.com>
2024-09-02 13:17:03 +0530

    method to create task to save task to file created

---
+++ src/task5/TaskService.java
@@ -0,0 +1,17 @@
+package task5;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TaskService {
+    private final List<Task> tasks = new ArrayList();
+
+    public void createTask(String title, String description) {
+        task3.Task newTask = new task3.Task(tasks.size() + 1, title, description);
+        tasks.add(newTask);
+        saveTasksToFile(tasks);
+        System.out.println(tasks.get(0));
+        System.out.println("Task created successfully!");
+    }
+
+}
